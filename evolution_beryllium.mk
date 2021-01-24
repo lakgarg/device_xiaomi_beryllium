d65b9ca3c75bb05c599d697ea0dec3a80b15b3df
Lakshay <lakshaygarg.in@gmail.com>
2021-01-24 18:20:53 +0000

    Adapt to EvolutionX
    Signed-off-by: Lakshay <lakshaygarg.in@gmail.com>

--- evolution_beryllium.mk
+++ evolution_beryllium.mk
@@ -6,14 +6,14 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common Evolution stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_WIFI_EXT := true
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := evolution_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
